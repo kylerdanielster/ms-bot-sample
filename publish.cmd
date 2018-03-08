5970ac1c50a050b770134452cde45a262836393a
Kyle Daniel <kyle.daniel@guidestone.org>
2018-03-08 16:06:57 -0600

    init

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=gs-ps-sample-bot-Web-Deploy.pubxml -p:Password=Y2wQXnuE1G0zT0HrBSieP1EG0lGiR0rw5hyDfzkrCZNNw5KHx6fLpQSjHeAe
+
